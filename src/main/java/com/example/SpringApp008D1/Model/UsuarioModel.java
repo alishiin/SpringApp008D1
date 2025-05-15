2b9a00f97819804a2a86bb25884861d92bc9b04c
alishiin <sa.vargaso@duocuc.cl>
2025-05-14 23:46:51 -0400

    Agregando package y clases

---
+++ src/main/java/com/example/SpringApp008D1/Model/UsuarioModel.java
@@ -0,0 +1,5 @@
+package com.example.SpringApp008D1.Model;
+
+public class UsuarioModel {
+    private String nombre;
+}
---
+++ src/main/java/com/example/SpringApp008D1/SpringApp008D1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringApp008D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp008D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApp008D1Application.class, args);
+	}
+
+}
